a1450b93e43ea673259accb58aae4299e157998e
jonathanorlen <pellondou.jo@gmail.com>
2020-08-20 15:28:59 +0700

    Updates

---
+++ precache-manifest.2cb1b5ceeaccb09db3aaa98faf96f7b3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed8d52a8d92988d183ebdefe7db4b70",
+    "url": "/staycation-frontend/index.html"
+  },
+  {
+    "revision": "b3b509f8bca832bbebe6",
+    "url": "/staycation-frontend/static/css/2.33d22cee.chunk.css"
+  },
+  {
+    "revision": "599b60c227844e360938",
+    "url": "/staycation-frontend/static/css/main.d728d7ba.chunk.css"
+  },
+  {
+    "revision": "b3b509f8bca832bbebe6",
+    "url": "/staycation-frontend/static/js/2.ce59d050.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/staycation-frontend/static/js/2.ce59d050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599b60c227844e360938",
+    "url": "/staycation-frontend/static/js/main.6a6b71a9.chunk.js"
+  },
+  {
+    "revision": "39aa4bba84b339b00f7a",
+    "url": "/staycation-frontend/static/js/runtime-main.3d3ef01d.js"
+  },
+  {
+    "revision": "c6ccd4740c8b875337693a033d847944",
+    "url": "/staycation-frontend/static/media/completed.c6ccd474.jpg"
+  },
+  {
+    "revision": "19fa22a679fb41aa197b518e0eb2d802",
+    "url": "/staycation-frontend/static/media/hero-frame.19fa22a6.jpg"
+  },
+  {
+    "revision": "3a77d2137cb27cc1b7a8bf3074e61d7d",
+    "url": "/staycation-frontend/static/media/hero.3a77d213.jpg"
+  },
+  {
+    "revision": "f8d2d21123e949a45b14415ef20d1a9a",
+    "url": "/staycation-frontend/static/media/ic_calendar.f8d2d211.svg"
+  },
+  {
+    "revision": "c6046ef6334b6feb346a4ad4bd9553df",
+    "url": "/staycation-frontend/static/media/ic_cities.c6046ef6.svg"
+  },
+  {
+    "revision": "909645524163bb92cefdb10fdf40fd91",
+    "url": "/staycation-frontend/static/media/ic_traveler.90964552.svg"
+  },
+  {
+    "revision": "d28b99377805505a33f962a38f6d6537",
+    "url": "/staycation-frontend/static/media/ic_treasure.d28b9937.svg"
+  },
+  {
+    "revision": "e1f52ffd6cefde4614a8547417fb990d",
+    "url": "/staycation-frontend/static/media/logo-bca.e1f52ffd.jpg"
+  },
+  {
+    "revision": "8c44d8bad4f296823c1f70bb36bfc4c6",
+    "url": "/staycation-frontend/static/media/logo-mandiri.8c44d8ba.jpg"
+  },
+  {
+    "revision": "1db4ba472cbf2d93d2a98489193d8b23",
+    "url": "/staycation-frontend/static/media/star1.1db4ba47.svg"
+  }
+]);
